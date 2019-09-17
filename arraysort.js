0ffd74f70a0a7e5d176a09e0e66a24caa055c6fb
Angom Naomi <angomnd@gmail.com>
2019-09-17 23:45:50 +0300

    Finding evens, odds and characters

---
+++ arraysort.js
@@ -0,0 +1,25 @@
+//<script src="sorting.js"></script>
+function arraySort(anArray) {
+    if (anArray%2===0){
+        return (anArray.push([evens]));
+        return ("Evens");
+    }
+    else if (anArray%2!==0){
+        return (anArray.push([odds]));
+        return ("Odds");
+    }
+    else{
+        return (anArray.push([chars]));
+        return ("Chars");
+    }
+
+}
+let anArray=[2,0,6,5,1,7,'z','a'];
+let evens=new Array();
+let odds=new Array();
+let chars=new Array();
+
+module.exports = arraySort;
+
+
+
